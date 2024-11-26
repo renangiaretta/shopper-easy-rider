8113c70eeb4f95fbc0fb96382f5a2b7e1a500d50
unknown <renan.giaretta@hotmail.com>
2024-11-26 10:25:01 -0300

    feat: generate Ride module with resource

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RideModule } from './modules/ride/ride.module';
 
 @Module({
-  imports: [],
+  imports: [RideModule],
   controllers: [AppController],
   providers: [AppService],
 })
