cc6721479d736ccab6f4bff3acadd902fc7de7a8
unknown <renan.giaretta@hotmail.com>
2024-11-26 11:37:17 -0300

    fix: add httpcode 200 on success response of estimate endpoint

--- backend-nestjs/src/modules/ride/ride.controller.ts
+++ backend-nestjs/src/modules/ride/ride.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { RideService } from './ride.service';
 import { EstimateRideDto } from './dto/estimate-ride.dto';
 
@@ -6,7 +6,7 @@ import { EstimateRideDto } from './dto/estimate-ride.dto';
 export class RideController {
 	constructor(private readonly rideService: RideService) {}
 	@Post('/estimate')
-	// @HttpCode(200)
+	@HttpCode(200)
 	estimateRide(@Body() estimateRideDto: EstimateRideDto) {
 		return this.rideService.estimateRide(estimateRideDto);
 	}
