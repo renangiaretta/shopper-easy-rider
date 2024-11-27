cf2336b9b2e88ef1d0199756148e984b83d07ed9
unknown <renan.giaretta@hotmail.com>
2024-11-27 03:23:16 -0300

    chore: add and configure Sass for style management

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src', 'scss')],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**'
+        }
+      ]
+    }
 };
 
 export default nextConfig;
