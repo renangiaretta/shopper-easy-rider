08f601bc9a23a5bb3325848ba2d026af28d02978
unknown <renan.giaretta@hotmail.com>
2024-11-27 05:15:12 -0300

    feat: create background image component

---
+++ frontend-nextjs/src/components/BackgroundImage/index.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Link } from 'react-scroll';
+import styles from './styles.module.sass';
+
+interface BackgroundImageProps {
+    title: string;
+    subtitle: string;
+    linkText: string;
+    linkTarget: string;
+}
+const BackgroundImage: React.FC<BackgroundImageProps> = ({ title, subtitle, linkText, linkTarget }: BackgroundImageProps): JSX.Element => {
+
+    return (
+        <div className={styles['background-image-container']}>
+            <div className={styles['content-overlay']}>
+                <h1 className={styles['title']}>{title}</h1>
+                <p className={styles['subtitle']}>{subtitle}</p>
+                <Link
+                    to={linkTarget}
+                    smooth={true}
+                    duration={500}
+                    className={styles['action-link']}>
+                    {linkText}
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default BackgroundImage;
