396e9ff87da9e728648c624a77ed61b6c4ae7054
unknown <renan.giaretta@hotmail.com>
2024-11-27 07:17:31 -0300

    feat: create ride options page

---
+++ frontend-nextjs/src/app/opcoes-de-viagem/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import Header from "@/components/Header";
+import { StoreWrapper } from "@/store/StoreWrapper";
+import Footer from "@/components/Footer";
+import './../../scss/main.sass'
+
+
+export const metadata: Metadata = {
+    title: "Easy Rider",
+    description: "O melhor está por vir. Aperte o cinto!",
+};
+
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="pt-br">
+            <body>
+                <StoreWrapper>
+                    <Header />
+                    {children}
+                    <Footer />
+                </StoreWrapper>
+            </body>
+        </html >
+    );
+}
