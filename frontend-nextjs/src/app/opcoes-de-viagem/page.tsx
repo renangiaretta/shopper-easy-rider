8e10447b344621bb1ff3c46aa7100b29225d3a50
unknown <renan.giaretta@hotmail.com>
2024-11-27 07:26:55 -0300

    feat: create StaticMap component

--- frontend-nextjs/src/app/opcoes-de-viagem/page.tsx
+++ frontend-nextjs/src/app/opcoes-de-viagem/page.tsx
@@ -1,9 +1,9 @@
 import MainComponent from '@/components/MainComponent';
-import { Element } from 'react-scroll';
+
 export default function RideOptions() {
     return (
         <MainComponent>
-            <p>oi</p>
+            <RideOptions />
         </MainComponent>
     );
 }
