e31aab9e5cb89a2e4d14ff50b0de2eb6c272e8a6
unknown <renan.giaretta@hotmail.com>
2024-11-27 20:06:12 -0300

    feat: create ride history page

---
+++ frontend-nextjs/src/app/historico-de-viagens/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import Header from "@/components/Header";
+import { StoreWrapper } from "@/store/StoreWrapper";
+import Footer from "@/components/Footer";
+import './../../scss/main.sass'
+
+
+export const metadata: Metadata = {
+    title: "Easy Rider",
+    description: "O melhor está por vir. Aperte o cinto!",
+};
+
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <StoreWrapper>
+            <Header />
+            {children}
+            <Footer />
+        </StoreWrapper>
+    );
+}
---
+++ frontend-nextjs/src/app/historico-de-viagens/page.tsx
@@ -0,0 +1,10 @@
+import MainComponent from '@/components/MainComponent';
+import RideOptions from '@/components/RideOptions';
+
+export default function RideHistoryPage() {
+    return (
+        <MainComponent>
+            <></>
+        </MainComponent>
+    );
+}
